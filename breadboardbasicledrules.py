48082d6d227dd7de5c3cb62fbdf3904fa82c0e10
SidSaucy <60280178+SidSaucy@users.noreply.github.com>
2020-02-01 10:27:50 -0800

    Add files via upload

---
+++ breadboardbasicledrules.py
@@ -0,0 +1,6 @@
+#rules for basic breadboard
+
+1. one wire from ground to negative on 'abcde' side
+2. one wire from 3.3v to positive on 'fghij' side
+3. if using code, one wire from gpio21 on 'abcde' side to same verical line as resistor
+4. resistor on same line as led on 'abcde' side connectedto different line on 'fghij' side
